2d22f7fbc63b3dc1234b605fdade50fe743f7887
mrs0nu <sonuyadav8850527644@gmail.com>
2025-05-10 11:22:12 +0530

    using time method
    try some time method

---
+++ p.java
@@ -0,0 +1,13 @@
+
+
+import java.time.*;
+
+public class p{
+    public class time {
+
+    }
+    public static void main(String args[]){
+        System.out.println(LocalTime.now());
+    System.out.println(LocalDateTime.now());
+    }
+}
