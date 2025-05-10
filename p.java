0e8dedf98b58c904356179e3441e483293038242
Sonu Yadav <sonuyadav8850527644@gmail.com>
2025-05-10 11:25:48 +0530

    Update p.java
    testing some git feature

--- p.java
+++ p.java
@@ -3,9 +3,7 @@
 import java.time.*;
 
 public class p{
-    public class time {
-
-    }
+  
     public static void main(String args[]){
         System.out.println(LocalTime.now());
     System.out.println(LocalDateTime.now());
