2c8ead611d97d460c432ab501d8bc0227e9442b5
mrs0nu <sonuyadav8850527644@gmail.com>
2025-05-12 16:27:18 +0530

    I am try some git commannd

--- p.java
+++ p.java
@@ -4,7 +4,10 @@ import java.time.*;
 
 public class p{
     public class time {
-
+        int a=8;
+        public static void main (){
+            System.err.println("Hi i  am git and github");
+        }
     }
     public static void main(String args[]){
         System.out.println(LocalTime.now());
